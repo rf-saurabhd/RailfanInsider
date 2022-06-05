c649b6d3996de53706bb081b5f52b37d709c17be
rf-saurabhd <saurabhdorle20598@gmail.com>
2022-06-05 17:14:10 +0530

    Adding Student.java file

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student{
+
+public static void main(String args[]){
+
+System.out.println("Hello World Welcome To GitHub");
+System.out.println("Even I have Started to learn few Commands");
+}
+
+}
