d433b77dcd81ccd664d4760af93760694d233e83
rf-saurabhd <saurabhdorle20598@gmail.com>
2022-06-05 10:50:33 +0530

    first ever commit

---
+++ Git.java
@@ -0,0 +1,9 @@
+public class Git{
+
+public static void main(String args[]){
+
+System.out.println("Local Working Directory");
+System.out.println("Stagging area");
+System.out.println("Local Repository");
+}
+}
